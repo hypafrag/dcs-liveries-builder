c6bda1159444174e07184d7fc0d63ac846aac68a
escobar <escobar@mosk.ru>
2020-07-01 01:37:22 +0700

    updated json format

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 from wand.image import Image
+from wand.exceptions import BlobError
 import os
 import json
 import argparse
@@ -13,7 +14,7 @@ args = argparser.parse_args()
 desc = json.load(args.desc_json)
 desc_dir = pathlib.Path(args.desc_json.name).parent
 livery_name = desc['name']
-textures_desc = desc['textures']
+materials_desc = desc['textures']
 livery_dir = args.out or pathlib.Path('dcs_liveries_builds') / livery_name
 try:
     os.makedirs(livery_dir)
@@ -26,16 +27,36 @@ os.chdir(desc_dir)
 print(f'name = "{livery_name}"', file=description_lua)
 print('livery = {', file=description_lua)
 
-for target in textures_desc:
-    texture_desc = textures_desc[target]
-    image = Image(filename=texture_desc[0])
-    image_name, _ = os.path.splitext(texture_desc[0])
-    texture = image.convert('dds')
-    texture.compression = texture_desc[1]
-    # texture.options['dds:weight-by-alpha'] = 'false'
-    # texture.options['dds:mipmaps'] = '0'
-    texture.save(filename=(livery_dir / image_name).with_suffix('.dds'))
-    print(f'  {{"{target}", DIFFUSE, "{image_name}", false}},', file=description_lua)
-    print(target)
+converted = set()
+
+def add_entry(node, texture_desc, material_property='DIFFUSE'):
+    texture_file, compression = texture_desc
+    image_name, _ = os.path.splitext(texture_file)
+    image_name = pathlib.Path(f'{image_name}_{compression}')
+    if texture_desc not in converted:
+        try:
+            image = Image(filename=texture_file)
+        except BlobError:
+            print(f'Failed to load {texture_desc} for {node}[{material_property}]')
+            exit(1)
+        print(f'Converting {texture_desc} for {node}[{material_property}]')
+        texture = image.convert('dds')
+        texture.compression = compression
+        # texture.options['dds:weight-by-alpha'] = 'false'
+        # texture.options['dds:mipmaps'] = '0'
+        texture.save(filename=(livery_dir / image_name).with_suffix('.dds'))
+        converted.add(texture_desc)
+    else:
+        print(f'Already converted {texture_desc} for {node}[{material_property}]')
+    print(f'  {{"{node}", {material_property}, "{image_name}", false}},', file=description_lua)
+
+for node in materials_desc:
+    material_desc = materials_desc[node]
+    if isinstance(material_desc, list):
+        add_entry(node, tuple(material_desc))
+    elif isinstance(material_desc, dict):
+        for material_property in material_desc:
+            add_entry(node, tuple(material_desc[material_property]),
+                      material_property=material_property)
 
 print('}', file=description_lua)
